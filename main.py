7f978a6c72d86e2f757eef507c583ffa2d2dde2f
toru1221 <torumia20@gmail.com>
2023-07-31 16:15:51 +0900

    1st commit

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import streamlit as st
+import numpy as np
+from PIL import Image
 import time
 
-st.title('初めてのWebアプリ')
 
-st.write('プログレスバーの表示')
-'START!!'
+st.title('初めてのWebアプリ')
 
 latest_iteration = st.empty()
 bar = st.progress(0)
 
 for i in range(100):
-    latest_iteration.text(f'読み込み率 {i+1} %')
+    latest_iteration.text(f'起動率： {i+1} %')
     bar.progress(i + 1)
     time.sleep(0.1)
 
-'Done!!!'
+img = Image.open('wallpaperbetter.jpg')
+st.image(img, caption='Hatsune Miku', use_column_width=True)
 
 left_column, right_column = st.columns(2)
 button = left_column.button('右カラムに文字を表示')
